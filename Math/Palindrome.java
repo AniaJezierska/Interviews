37422885820e870025bb36a7088b6088bdff3b2c
AniaJezierska <annjez.dev@gmail.com>
2023-07-31 13:42:15 +0200

    Create Palindrome.java

---
+++ Math/Palindrome.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+/**
+ * CLASS METHOD SumTwoNumbers...
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+ *
+ * Example:
+ * Input: x = 121
+ * Output: true
+ *
+ * Input: x = -121
+ * Output: false
+ *
+ * Solution:
+ * Good idea would be reverting the number itself, and then compare the number
+ * with original number, if they are the same, then the number is a palindrome.
+ * To avoid the overflow issue of the reverted number, what if we only revert
+ * half of the int\text{int}int number?
+ */
+
+public class Palindrome {
+
+    public static boolean isPalindrome(int number) {
+        // we can return false for all negative numbers..
+        if (number < 0 || (number != 0 && number%10 == 0))
+            return false;
+        int reversed = 0;
+        while (number > reversed){
+            // we get the last digit by %10 for ex. 1 for 1221
+            reversed = reversed*10 + number%10;
+            // to get the second to the last digit, we need to remove the last digit by /10..
+            // and %10 for ex. 2 for 122 (in the next rotation of the loop)
+            number = number/10;
+        }
+        return (number == reversed || number == reversed/10);
+    }
+
+    public static void main(String[] args) {
+
+        int x = 1221;
+        System.out.println("Is palindrome? " + (isPalindrome(x)));
+
+    }
+}
