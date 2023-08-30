8b6815eb6b74c577e8aaa8343fd2a01cd7f6b694
AniaJezierska <annjez.dev@gmail.com>
2023-08-30 09:05:54 +0200

    Create RomanToInt.cpp

---
+++ HashMap/RomanToInt.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int romanToInt(string s) {
+
+    unordered_map<char, int> map;
+
+    map['I'] = 1;
+    map['V'] = 5;
+    map['X'] = 10;
+    map['L'] = 50;
+    map['C'] = 100;
+    map['D'] = 500;
+    map['M'] = 1000;
+
+    int result = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        if(map[s[i]] < map[s[i + 1]]){
+            result -= map[s[i]];
+        }
+        else{
+            result += map[s[i]];
+        }
+    }
+    return result;
+}
