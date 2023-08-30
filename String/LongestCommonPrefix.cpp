1c65c9d2a96be5df5af55dd27adb9b841caffa58
AniaJezierska <annjez.dev@gmail.com>
2023-08-30 09:11:27 +0200

    Create LongestCommonPrefix.cpp

---
+++ String/LongestCommonPrefix.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+
+    if (strs.empty()) {
+        return "";
+    }
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.length() - 1);
+        }
+    }
+    return prefix;
+}
