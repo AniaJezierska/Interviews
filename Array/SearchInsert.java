86347e3b753788c66704c2b19f7fb9c5002a38f8
AniaJezierska <annjez.dev@gmail.com>
2023-10-04 21:05:35 +0200

    Create SearchInsert.java

---
+++ Array/SearchInsert.java
@@ -0,0 +1,41 @@
+/**
+ * Given a sorted array of distinct integers and a target value,
+ * return the index if the target is found. If not, return the index
+ * where it would be if it were inserted in order.
+ */
+
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        SearchInsert searchInsertObj = new SearchInsert();
+
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
+        System.out.println(searchInsertObj.searchInsert(nums1, target1)); // Output: 2
+
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+        System.out.println(searchInsertObj.searchInsert(nums2, target2)); // Output: 1
+
+        int[] nums3 = {1, 3, 5, 6};
+        int target3 = 7;
+        System.out.println(searchInsertObj.searchInsert(nums3, target3)); // Output: 4
+    }
+}
