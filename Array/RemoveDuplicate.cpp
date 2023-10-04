bc2a5860406585d20478dd603ca23133925d4217
AniaJezierska <annjez.dev@gmail.com>
2023-10-04 20:58:26 +0200

    Create RemoveDuplicate.cpp

---
+++ Array/RemoveDuplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int insertIndex = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+};
